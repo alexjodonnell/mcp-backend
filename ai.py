9efbb29fe3fec95b1d9ec56b8dab2adb5b5e200d
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-03-09 15:01:48 -0400

    updated costs

--- ai.py
+++ ai.py
@@ -18,7 +18,19 @@ class AI:
         self.hub_capacity = parameters['hub_capacity']
         self.mining_rate = parameters['mining_rate']
         self.ms_per_week = parameters['ms_per_week']
-        self.costs = parameters['costs']
+
+        costs = parameters['costs']
+        self.build_cost = costs['build']['rate']
+        self.build_weeks = costs['build']['weeks']
+
+        self.deploy_cost = costs['deploy']['rate']
+        self.deploy_weeks = costs['deploy']['weeks']
+
+        self.ship_cost = costs['ship']['rate']
+        self.ship_weeks = costs['ship']['weeks']
+
+        self.move_cost = costs['build']['rate']
+        self.move_weeks = costs['build']['weeks']
 
         self.map = Map(parameters['rows'], parameters['cols'])
 
@@ -28,16 +40,22 @@ class AI:
 
         alg = Thread(target=self.algorithm)
         alg.start()
+
+        getter.join()
         alg.join()
         print('Finished')
 
     def get_data(self):
+        print(self.start_epoch)
         while True:
             next_epoch = self.start_epoch
             if utils.current_epoch() > next_epoch:
                 prospect_report = api.prospect_report()
                 next_epoch += WEEK * 78
                 self.map.append(prospect_report['report'])
+
+                status_report = api.status_report()
+                print(status_report['team']['week'])
 
     def algorithm(self):
         pass
